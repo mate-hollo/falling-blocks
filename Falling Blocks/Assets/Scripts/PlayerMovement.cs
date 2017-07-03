d4b9b7d09cbfc3d5a840cbdc50cd2fb581563fbc
mate-hollo <hollo89@gmail.com>
2017-07-03 19:41:14 +0200

    created
    added player movement and edge detection

---
+++ Falling Blocks/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	float screenHalfWidthInWorldUnits;
+
+	void Start () {
+		float halfPlayerWidth = transform.localScale.x / 2;
+		screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+
+	}
+	
+	void Update () {
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+		Vector2 moveAmount = input.normalized * speed * Time.deltaTime;
+		transform.Translate(moveAmount);
+
+
+		if (transform.position.x < -screenHalfWidthInWorldUnits)
+			transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+
+		if (transform.position.x > screenHalfWidthInWorldUnits)
+			transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+
+
+
+
+		//transform.position = camera.ViewportToWorldPoint(new Vector3(1f, -3.8f, 0));
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
